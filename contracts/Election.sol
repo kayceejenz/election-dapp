aeef73fdb517aa1741b82dc8d62eb1ca2975c53a
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-01-30 00:26:32 +0100

    initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.22;
+
+contract Election{
+
+    string public candidate;
+    // Store candiate
+    // Read candidate
+    // Constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+} 
